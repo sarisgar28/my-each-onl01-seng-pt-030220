9de5eed5420f5ab11873dc22169516c7fe638f4f
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-16 20:19:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,6 @@ def my_each(collection)
  collection = 0 
   collection do |index|
      return "1,2,3,4"
-     while my_each == 0 
-     my_each(words) do |word|
-       
-       puts word 
     end
 end
 
